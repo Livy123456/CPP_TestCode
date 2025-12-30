a24c2cc7f2f698c7f3b1ad9f92796a3904c295c5
Livy123456 <zf1602445854@163.com>
2025-12-31 06:42:47 +0800

    【代码参考来源（教程或书籍）】：参考boost库单例实现 【具体主题】：设计模式——单例 【csdn】：无

---
+++ 01_VS2022_TestCodes/03_DesignPattern/01_Singleton/Singleton.h
@@ -0,0 +1,77 @@
+#ifndef _SINGLETON_H
+#define _SINGLETON_H
+
+#include <iostream>
+
+template <typename T>
+class CSingleton
+{
+protected:
+	CSingleton() { std::cout << "singleton constructor\n"; }
+	CSingleton(const CSingleton& singleton);
+	CSingleton& operator=(const CSingleton& singleton);
+	~CSingleton() { std::cout << "singleton destructor\n"; }
+
+public:
+	//! 获取单例实例指针
+	static T* GetInstance()
+	{
+		if (nullptr == s_pInstance)
+		{
+			//Autolock
+			if (nullptr == s_pInstance)
+			{
+				s_pInstance = new T();
+			}
+		}
+		m_Deleter.do_nothing();
+		return s_pInstance;
+	}
+
+	//! 尝试获取单例实例指针，在释放的时候，已经释放了就不要创建实例了
+	//! return 单例实例指针
+	static T* TryGetInstance()
+	{
+		return s_pInstance;
+	}
+
+	//! 删除单例实例
+	static void ReleaseInstance()
+	{
+		if (nullptr != s_pInstance)
+		{
+			delete s_pInstance;
+			s_pInstance = nullptr;
+		}
+	}
+
+private:
+	//! 负责单例的资源初始化和释放
+	class CDeleter
+	{
+	public:
+		CDeleter()	//< 不管有没有人用单例实例，都会先创建出来
+		{
+			CSingleton<T>::GetInstance();
+		}
+		~CDeleter()
+		{
+			CSingleton<T>::ReleaseInstance();
+		}
+		inline void do_nothing() const {};
+	};
+
+private:
+	//! 单例实例指针
+	static T* s_pInstance;
+	//! 单例删除器
+	static CDeleter m_Deleter;
+};
+
+template <typename T>
+T* CSingleton<T>::s_pInstance = nullptr;
+
+template <typename T>
+typename CSingleton<T>::CDeleter CSingleton<T>::m_Deleter; //< 程序一运行就先调用自己的构造函数，对单例进行初始化
+
+#endif	//< _SINGLETON_H
---
+++ 01_VS2022_TestCodes/03_DesignPattern/01_Singleton/test.cpp
@@ -0,0 +1,43 @@
+
+#include <cstdio>
+#include "Singleton.h"
+
+namespace jj01
+{
+	class CPerson : public CSingleton<CPerson>
+	{
+		friend class CSingleton<CPerson>;	//< 因为要构建CPerson，但CPerson构造是私有的
+
+		//! 私有
+		CPerson() { std::cout << "CPerson constructor\n"; }
+		~CPerson() { std::cout << "CPerson destructor\n"; } //< 无法直接析构，需要通过单例析构
+		CPerson(const CPerson& per);
+		CPerson& operator=(const CPerson& per);
+
+	public:
+		void SayHello()
+		{
+			printf("Hello, I am a singleton person instance.\n");
+		};
+	};
+
+	void Test01()
+	{
+		//! 获取单例指针
+		CPerson* p = CPerson::GetInstance();
+		p->SayHello();
+		std::cout << std::hex << (void*)p << std::endl;
+
+		//! 释放单例指针
+		CPerson::ReleaseInstance();
+	}
+}
+
+
+int main(void)
+{
+	jj01::Test01();
+
+	getchar();
+	return 0;
+}
